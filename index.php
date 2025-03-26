dbfab5105a5ee6a827c55b7f0f60b6badbe2e594
rawrpit <1054@htl.rennweg.at>
2025-03-26 09:37:34 +0100

    description

--- index.php
+++ index.php
@@ -6,9 +6,9 @@ use Arpit\Mvc\HotelList;
 
 $hotelList = new HotelList();
 
-$hotelList->addHotel(new HotelClass("Venetian Resort Hotel", "Cute and luxurious hotel with Venetian theme.", "images/hotel1.png"));
-$hotelList->addHotel(new HotelClass("Caesars Palace", "Crazy vibes, entertainment, and opulence.", "images/hotel2.jpg"));
-$hotelList->addHotel(new HotelClass("Bellagio", "Besteeeee! Famous for its fountain shows.", "images/hotel3.jpg"));
+$hotelList->addHotel(new HotelClass("Venetian Resort Hotel", "Cute and butiful an richhyyyyyyyyyyyyy.", "images/hotel1.png"));
+$hotelList->addHotel(new HotelClass("Caesars Palace", "Crazy vibes, chill idk blablabla eypensive.", "images/hotel2.jpg"));
+$hotelList->addHotel(new HotelClass("Bellagio", "Besteeeee! Fsdofjsssssssssgows.", "images/hotel3.jpg"));
 
 $template = file_get_contents(__DIR__ . "/template.html");
 
